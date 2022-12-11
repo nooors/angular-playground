9eab5a7ab830d0a46e79e1cf4b70dbfb2989ed47
Jose Nortes <nooors@gmail.com>
2022-12-11 11:27:36 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  // production: true,
+  //baseUrl: //'http://localhost:8080/api', //'https://localhost:7143/api',
+};
